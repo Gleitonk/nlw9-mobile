bf02d1d77a88a941734c227b18db20616e6d0429
Gleiton <kgleiton@hotmail.com>
2022-09-18 22:20:17 -0300

    Adicionado cards screen game

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
@@ -69,8 +69,20 @@ export function Game() {
                     data={duos}
                     keyExtractor={item => item.id}
                     renderItem={({ item }) =>
-                        <DuoCard data={duos[0]} />
+                        <DuoCard
+                            data={duos[0]}
+                            onConnect={() => { }}
+                        />
                     }
+                    horizontal
+                    style={styles.containerList}
+                    contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+                    showsHorizontalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.emptyList}>
+                            Não há anúncios publicados ainda.
+                        </Text>
+                    )}
                 />
 
             </SafeAreaView>
